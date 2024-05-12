406637959bc95fc381ed4ee3140d8a02c80e377d
Pandlop <jc.lopezc1@uniandes.edu.co>
2024-05-12 17:18:52 -0500

    sidenote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SurveyComponent } from './survey/survey.component';
@@ -12,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { SurveyApiService } from './survey/surveyApi.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin/admin.component';
+
 
 @NgModule({
   declarations: [					
@@ -32,7 +35,7 @@ import { AdminComponent } from './admin/admin.component';
       timeOut: 10000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
-    }),
+    })
   ],
   providers: [SurveyApiService],
   bootstrap: [AppComponent]
